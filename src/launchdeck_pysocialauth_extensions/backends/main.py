621616791ea7a2e26756978f83439092cb6e5d0d
Jeremy Sanders <23668453+jeremypng@users.noreply.github.com>
2025-04-28 08:36:17 -0500

    work on redirect_uri

--- src/launchdeck_pysocialauth_extensions/backends/main.py
+++ src/launchdeck_pysocialauth_extensions/backends/main.py
@@ -1,11 +1,4 @@
-import logging
 from social_core.backends.azuread_tenant import AzureADTenantOAuth2, AzureADV2TenantOAuth2
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
-logger.addHandler(handler)
 
 class LaunchDeckAzureADTenantOAuth2(AzureADTenantOAuth2):
     name = "launchdeck-azuread-tenant-oauth2"
@@ -16,7 +9,7 @@ class LaunchDeckAzureADTenantOAuth2(AzureADTenantOAuth2):
         self.redirect_uri = self.strategy.absolute_uri(self.redirect_uri)
     
 
-class LaunchDeckAzureADV2TenantOAuth2(AzureADTenantOAuth2):
+class LaunchDeckAzureADV2TenantOAuth2(AzureADV2TenantOAuth2):
     name = "launchdeck-azuread-v2-tenant-oauth2"
 
     def __init__(self, *args, **kwargs):
