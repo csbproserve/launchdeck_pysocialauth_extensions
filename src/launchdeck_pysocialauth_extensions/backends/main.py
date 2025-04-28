ae1f5301c0c6e72d95f2b32a85cc8b90f8ffd561
Jeremy Sanders <23668453+jeremypng@users.noreply.github.com>
2025-04-28 08:08:23 -0500

    work on redirect_uri

--- src/launchdeck_pysocialauth_extensions/backends/main.py
+++ src/launchdeck_pysocialauth_extensions/backends/main.py
@@ -5,8 +5,6 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 class LaunchDeckAzureADTenantOAuth2(AzureADTenantOAuth2):
@@ -24,16 +22,16 @@ class LaunchDeckAzureADTenantOAuth2(AzureADTenantOAuth2):
     def request_access_token(self, *args, **kwargs):
         logger.critical("Requesting access token with args:", args, "and kwargs:", kwargs)
         access_token =  super().request_access_token(*args, **kwargs)
-        print("Access token received:", access_token)
+        logger.critical("Access token received:", access_token)
         return access_token
     
     def user_data(self, *args, **kwargs):
         response = super().user_data(*args, **kwargs)
-        print("User data received:", response)
+        logger.critical("User data received:", response)
         return response
     
     def auth_complete(self, *args, **kwargs):
-        print("Completing authentication with args:", args, "and kwargs:", kwargs)
+        logger.critical("Completing authentication with args:", args, "and kwargs:", kwargs)
         auth_repsonse = super().auth_complete(*args, **kwargs)
-        print("Authentication response:", auth_repsonse)
+        logger.critical("Authentication response:", auth_repsonse)
         return auth_repsonse
